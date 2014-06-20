2f657adc9d04060ab9271af4a4c2087249d08eb2
Amelia Downs <downs.amelia@gmail.com>
2014-06-20 14:45:06 -0700

    models need testing

--- source/app/models/task.rb
+++ source/app/models/task.rb
@@ -3,5 +3,14 @@ require 'sqlite3'
 
 class Task < ActiveRecord::Base
   belongs_to :goal
+
+
+  def self.complete(task_id)
+    my_task = Task.find(task_id)
+    my_task.completed = true
+    my_task.save
+    my_goal = Goal.find(my_task.goal_id)
+    my_goal.hours_completed += my_task.hours
+  end
 end
 
