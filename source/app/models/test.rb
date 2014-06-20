d94bd8497d3601bb10fc2f8e4e9e20c31bf6fb72
Amelia Downs <downs.amelia@gmail.com>
2014-06-20 15:23:04 -0700

    complete works

--- source/app/models/test.rb
+++ source/app/models/test.rb
@@ -1,14 +1,16 @@
 require_relative 'goal'
 
 
-end_date = Time.now + 6*24*60*60
+# end_date = Time.now + 6*24*60*60
 
-goal_info = { title: "Juggle",
-  end_date: end_date,
-  hours_needed: 20,
-  hours_completed: 0,
-  weekday: false,
-  weekend: true
-}
+# goal_info = { title: "Juggle",
+#   end_date: end_date,
+#   hours_needed: 20,
+#   hours_completed: 0,
+#   weekday: true,
+#   weekend: true
+# }
 
-Goal.make_a_goal(goal_info)
+# Goal.make_a_goal(goal_info)
+
+
